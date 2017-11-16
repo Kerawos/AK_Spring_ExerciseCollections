dcf7d6b6a717ffb57a951b5c609ab24229ef2f75
Kerawos <Maresowa@gmail.com>
2017-11-16 14:20:54 +0100

    Initial commit

---
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseCollections/AkSpringExerciseCollectionsApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.AK_Spring_ExerciseCollections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkSpringExerciseCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkSpringExerciseCollectionsApplication.class, args);
+	}
+}
