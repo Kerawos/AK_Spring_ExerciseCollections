14aa6895b2fab1a86568ca132113789d6224ca0b
Kerawos <Maresowa@gmail.com>
2017-12-04 14:56:46 +0100

    @add comments

--- src/main/java/pl/akademiakodu/AK_Spring_ExerciseCollectionsMaps/AkSpringExerciseCollectionsMapsApplication.java
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseCollectionsMaps/AkSpringExerciseCollectionsMapsApplication.java
@@ -3,6 +3,9 @@ package pl.akademiakodu.AK_Spring_ExerciseCollectionsMaps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring boot
+ */
 @SpringBootApplication
 public class AkSpringExerciseCollectionsMapsApplication {
 
